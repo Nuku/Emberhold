17651c4b519aa2f0eb2fe05b025c46bc7ff83306
David Silver <dms.silver@gmail.com>
2026-09-05 20:16:24 -0700

    Increase Silence Trial knowledge reserve again

--- js/game.js
+++ js/game.js
@@ -1021,7 +1021,7 @@ function setOut(trialId = null) {
   if (trialId === 'silence') {
     // Silence requires the full prerequisite chain plus Metallurgy, but
     // knowledge production is disabled for the entire trial.
-    state.res.knowledge = 1600;
+    state.res.knowledge = 1700;
     state.seen.knowledge = true;
   }
   if (up.caravans) {
